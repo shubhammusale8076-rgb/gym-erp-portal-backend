08552248092327ce8142c2233feea2852972f36c
Shubham807621 <shubhammusale111@gmail.com>
2026-04-02 13:13:23 +0530

    first commit

---
+++ src/main/java/com/gym/Elite/Gym/EliteGymApplication.java
@@ -0,0 +1,13 @@
+package com.gym.Elite.Gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliteGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/Elite/Gym/EliteGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.Elite.Gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
